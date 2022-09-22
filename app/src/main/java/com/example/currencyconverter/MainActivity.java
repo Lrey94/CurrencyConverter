b76e789f154fb78bcd5a24149ce3b4a787803740
Lrey94 <110589469+Lrey94@users.noreply.github.com>
2022-09-22 10:26:55 +0100

    Second Edition

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void onBtnClick (View view){
+        EditText edtCurrencyConversion = findViewById(R.id.editAmountToConvert);
+        String amountInGBP = edtCurrencyConversion.getText().toString();
+        Double amountInGBPDouble =Double.parseDouble(amountInGBP);
+        Double amountInUSDDouble = amountInGBPDouble * 1.13;
+
+        Toast.makeText(this, amountInGBP + "in dollars is " + amountInUSDDouble, Toast.LENGTH_SHORT).show();
 
     }
 }
